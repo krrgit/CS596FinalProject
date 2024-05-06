9faf1e1fcb1c45e9a8542f4f81334058de0cbdff
Morgan Simon <msimon1192@sdsu.edu>
2024-05-06 11:05:28 -0700

    Changed spawn position ranges for gems

--- Assets/Scripts/Gems/GemSpawner.cs
+++ Assets/Scripts/Gems/GemSpawner.cs
@@ -7,8 +7,13 @@ public class GemSpawner : MonoBehaviour
     public float dropInterval = 10f;
     private float nextDropTime;
 
+    public Vector2 minSpawnPosition;
+    public Vector2 maxSpawnPosition;
+
      void Start()
     {
+        minSpawnPosition = new Vector2(0f, 0f); 
+        maxSpawnPosition = new Vector2(4f, 7f); 
         // commented out for testing so the sphere's drop from the start
         // drop the first sphere after dropInterval time
         // nextDropTime = Time.time + dropInterval;
@@ -26,17 +31,15 @@ public class GemSpawner : MonoBehaviour
     void DropGem()
     {
         // random x position within range of screen width
-        float randomX = Random.Range(0f, Screen.width);
-        
-        // screen position to world position
-        Vector3 screenPosition = new Vector3(randomX, Screen.height, 10f);
-        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
-        
+        float randomX = Random.Range(minSpawnPosition.x, maxSpawnPosition.x);
+        float randomZ = Random.Range(minSpawnPosition.y, maxSpawnPosition.y);
+        Vector3 spawnPosition = new Vector3(randomX, transform.position.y, randomZ);
+
         // maintain z position of gem
-        worldPosition.z = transform.position.z;
+        // worldPosition.z = transform.position.z;
         
         // instantiate gem
-        Instantiate(gemPrefab, worldPosition, Quaternion.identity);
+        Instantiate(gemPrefab, spawnPosition, Quaternion.identity);
     }
 
 }
